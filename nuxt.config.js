d02a40e367a61db8aa430453b1848441a3805662
sunny <seonyoung.jeon@injeinc.co.kr>
2023-05-30 14:31:42 +0900

    기본 sentry 설정 및 테스트

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,8 +30,10 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
-
+  modules: ['@nuxtjs/sentry'],
+  sentry: {
+    dsn: '',
+  },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-}
+};
--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,19 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from 'vue';
 
 export default Vue.extend({
   name: 'IndexPage',
-})
+  async asyncData({ $sentry }) {
+    try {
+      setTimeout(() => {
+        throw new Error('에러 발생...!!');
+      }, 1000);
+    } catch (error) {
+      console.log(error);
+      $sentry.captureException(error);
+    }
+  },
+});
 </script>
