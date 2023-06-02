814b492caed1cffba957a63cac3e51ccae244736
sunny <seonyoung.jeon@injeinc.co.kr>
2023-06-02 14:10:19 +0900

    센트리 릴리즈 업로드 후 소스맵 삭제 추가

--- nuxt-configs/sentry.ts
+++ nuxt-configs/sentry.ts
@@ -1,28 +1,35 @@
 import type { ModuleOptions } from '@nuxtjs/sentry';
 const { execSync } = require('child_process');
+// @ts-ignore
+const isRunNuxtBuild = process.env.npm_lifecycle_script.includes('nuxt build');
 const SENTRY_STATUS = process.env.SENTRY || 'off';
 
 const sentryConfig: ModuleOptions =
   SENTRY_STATUS === 'on'
     ? {
         dsn: '',
-        publishRelease: {
-          authToken: '',
-          org: '',
-          project: 'nuxt-sample',
-          include: ['./dist'],
-          ignore: ['node_modules'],
-          release: (() => {
-            try {
-              return execSync('git describe --abbrev=0 --tags')
-                .toString()
-                .trim();
-            } catch (e) {
-              return `nuxt-sample-${new Date().toISOString()}`;
-            }
-          })(),
-        },
         sourceMapStyle: 'source-map',
+        disabled: true,
+        config: {},
       }
     : {};
+
+if (SENTRY_STATUS && isRunNuxtBuild) {
+  console.log('들어오냐?');
+  sentryConfig.publishRelease = {
+    authToken: '',
+    org: '',
+    project: 'javascript-vue6',
+    include: ['./dist'],
+    ignore: ['node_modules'],
+    release: (() => {
+      try {
+        return execSync('git describe --abbrev=0 --tags').toString().trim();
+      } catch (e) {
+        return `nuxt-sample4-${new Date().toISOString()}`;
+      }
+    })(),
+  };
+}
+console.log('config ==>>>', sentryConfig);
 export const sentry = sentryConfig;
